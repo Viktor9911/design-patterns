841dc46e9eacd5196fe6ca5653709aaed25d802c
Viktor9911 <80538072+Viktor9911@users.noreply.github.com>
2021-05-05 13:39:08 -0700

    Delete ObserverHomework directory

--- ObserverHomework/src/observer/Observable.java
+++
@@ -1,9 +0,0 @@
-package observer;
-
-public interface Observable {
-	
-	void buyMovie(Observer movie);
-	void returnMovie(Observer movie);
-	void NotifyMovies();
-	String getUpdate();
-}
--- ObserverHomework/src/observer/Observer.java
+++
@@ -1,6 +0,0 @@
-package observer;
-
-public interface Observer {
-	void update();
-	void setCustomer(Observable customer);
-}
